6b55cd9c9d648a16cf95a021717273672ca8f4f4
Shun Matsumoto <shun.matsumoto@pixta.co.jp>
2012-04-25 22:26:30 +0900

    update

--- lib/generators/omniauth/scaffold/scaffold_generator.rb
+++ lib/generators/omniauth/scaffold/scaffold_generator.rb
@@ -16,12 +16,10 @@ module Omniauth
         copy_file "config/initializers/omniauth.rb", "config/initializers/omniauth.rb"
         copy_file "config/initializers/local_setting.rb", "config/initializers/local_setting.rb"
         copy_file "config/initializers/constants.rb", "config/initializers/constants.rb"
-#        copy_file "config/routes.rb", "config/routes.rb"
         
-        content = File.read( "config/routes.rb", encoding: Encoding::UTF_8 )
-        insert_into_file "config/routes.rb", content.to_s, after: "do\n"
-        insert_into_file "config/routes.rb", "root :to => 'top#index'", after: "'welcome#index'\n"
-        insert_into_file "config/routes.rb", "match ':controller(/:action(/:id))(.:format)'", after: "  # Note: This route will make all actions in every controller accessible via GET requests.\n"
+        insert_into_file "config/routes.rb", "  match \"/auth/:provider/callback\" => \"sessions#callback\"\n  match \"/auth/failure\" => \"sessions#failure\"\n  match \"/logout\" => \"sessions#destroy\", :as => :logout\n", after: "# first created -> highest priority.\n"
+        insert_into_file "config/routes.rb", "  root :to => 'top#index'\n", after: "# root :to => 'welcome#index'\n"
+        insert_into_file "config/routes.rb", "  match ':controller(/:action(/:id))(.:format)'\n", after: "# match ':controller(/:action(/:id))(.:format)'\n"
         
         copy_file "db/migrate/create_users.rb", "db/migrate/20000101000000_create_users.rb"
         copy_file "app/models/user.rb", "app/models/user.rb"
