af15735f0cd6d96efae6052a931def21af7ba27a
Shun Matsumoto <shun.matsumoto@pixta.co.jp>
2012-04-25 22:19:59 +0900

    update

--- lib/generators/omniauth/scaffold/scaffold_generator.rb
+++ lib/generators/omniauth/scaffold/scaffold_generator.rb
@@ -7,13 +7,21 @@ module Omniauth
       source_root File.expand_path("../templates", __FILE__)
       desc "This generator installs Twitter Bootstrap to Asset Pipeline"
       
+      attr_reader :app_name
+      
       def generate_scaffold
+        app = ::Rails.application
+        @app_name = app.class.to_s.split("::").first
+
         copy_file "config/initializers/omniauth.rb", "config/initializers/omniauth.rb"
         copy_file "config/initializers/local_setting.rb", "config/initializers/local_setting.rb"
         copy_file "config/initializers/constants.rb", "config/initializers/constants.rb"
+#        copy_file "config/routes.rb", "config/routes.rb"
         
-        content = File.read( "config/routes.rb" )
-        insert_into_file "config/routes.rb", content, after: "routes.draw do\n"
+        content = File.read( "config/routes.rb", encoding: Encoding::UTF_8 )
+        insert_into_file "config/routes.rb", content.to_s, after: "do\n"
+        insert_into_file "config/routes.rb", "root :to => 'top#index'", after: "'welcome#index'\n"
+        insert_into_file "config/routes.rb", "match ':controller(/:action(/:id))(.:format)'", after: "  # Note: This route will make all actions in every controller accessible via GET requests.\n"
         
         copy_file "db/migrate/create_users.rb", "db/migrate/20000101000000_create_users.rb"
         copy_file "app/models/user.rb", "app/models/user.rb"
