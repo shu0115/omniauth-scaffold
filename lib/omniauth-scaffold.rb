8ff6e3feb0c4b2cce4995974f6bf68ddbe12dab0
shu0115 <s.matsumoto0115@gmail.com>
2012-12-18 18:22:12 +0900

    Update

--- lib/omniauth-scaffold.rb
+++ lib/omniauth-scaffold.rb
@@ -43,7 +43,7 @@ module Omniauth
 
         # ----- application.rb ----- #
         content = "    config.time_zone = 'Tokyo'\n"
-        content += "    config.active_record.default_timezone = :local\n"
+#        content += "    config.active_record.default_timezone = :local\n"
         insert_into_file( "config/application.rb", content.force_encoding('ASCII-8BIT'), after: "# config.time_zone = 'Central Time (US & Canada)'\n" )
         insert_into_file( "config/application.rb", "    config.i18n.default_locale = :ja\n", after: "# config.i18n.default_locale = :de\n" )
         content = "\n    # For Heroku\n"
