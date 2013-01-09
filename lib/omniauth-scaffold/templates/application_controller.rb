c7cd6fc08a48a79ab3bc592c5142e2dcb9fc687f
shu0115 <s.matsumoto0115@gmail.com>
2013-01-09 16:23:50 +0900

    Update

--- lib/omniauth-scaffold/templates/application_controller.rb
+++ lib/omniauth-scaffold/templates/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < ActionController::Base
   # current_user #
   #--------------#
   def current_user
-    @current_user ||= (User.where( id: session[:user_id] ).first || User.new)
+    @current_user ||= User.where( id: session[:user_id] ).first
   end
   helper_method :current_user
 end
